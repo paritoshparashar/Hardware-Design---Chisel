9ab84c93274567b7ac46a2ae378a5d7dfadc6510
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-06-19 20:53:09 +0200

    Implemented The complete circuit, but with errors

--- src/main/scala/arithmetic/Divider.scala
+++ src/main/scala/arithmetic/Divider.scala
@@ -16,44 +16,70 @@ class Divider(bitWidth: Int) extends Module {
     val remainder   =   RegInit(0.U(bitWidth.W))       //current remainder
     val quotient    =   RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
     val divisor     =   RegInit(0.U(bitWidth.W))         //divisor
+    val dividend     =   RegInit(0.U(bitWidth.W))         //divisor
 
     val stepI       =   RegInit ((bitWidth-1).U(log2Ceil(bitWidth).W));
 
-    
-    when (io.start) {
+    val start :: calculate :: done :: Nil = Enum(3)
+    val state = RegInit(start);
 
-        val intermediateRem =  (remainder << 1.U ) + io.dividend(stepI)  // R' = 2*R + A[i];
-
-        /* 
-        if (R' == B) {
-            Q[i] = 1;
-            R = 0;
-        }
-        else {
-            Q[i] = 0;
-            R = R';
-        }
-        */
-        quotient(stepI) := Mux((intermediateRem === io.divisor) , 1.U , 0.U)
-        remainder := Mux((intermediateRem === io.divisor) , 0.U , intermediateRem) 
-            // Quotient
-
-
-            // Remainder
+    switch (state) {
         
+        is (start) {
 
-
-        when (stepI === 0.U) {
-            io.start := false.B;
+            when (io.start) {
+                divisor     :=  io.divisor
+                dividend    :=  io.dividend
+                state       :=  calculate
+            }
         }
-        .otherwise {
-            stepI := stepI - 1.U
+
+        is (calculate) {
+
+            when (io.start) {
+
+                val intermediateRem =  (remainder << 1.U ) + io.dividend(stepI)  // R' = 2*R + A[i];
+
+                /* 
+                if (R' == B) {
+                    Q[i] = 1;
+                    R = 0;
+                }
+                else {
+                    Q[i] = 0;
+                    R = R';
+                }
+                */
+                val remEqualToDivident = (intermediateRem === io.divisor);
+                quotient(stepI) := Mux(remEqualToDivident , 1.U , 0.U)
+                remainder := Mux(remEqualToDivident , 0.U , intermediateRem) 
+
+                when (stepI === 0.U) {
+                    
+                    state := done
+                }
+                .otherwise {
+                    stepI := stepI - 1.U
+                }
+
+            }
+            .otherwise {
+                state   :=  done
+            }  
+
         }
 
-    } .otherwise {
-        io.remainder    :=  remainder;
-        io.quotient     :=  quotient.asUInt;
-        io.done         :=  true.B;
+        is (done) {
+
+                io.remainder    :=  remainder;
+                io.quotient     :=  quotient.asUInt;
+                io.done         :=  true.B;
+
+            when (io.start) {
+                state   :=  start
+            }
+            
+        }
 
     }
 }
