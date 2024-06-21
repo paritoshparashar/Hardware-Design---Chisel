f627e5758203ca070c2609c42d844fbbe1001889
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-06-21 18:52:18 +0200

    Working module of Divider circuit

--- src/main/scala/arithmetic/Divider.scala
+++ src/main/scala/arithmetic/Divider.scala
@@ -13,79 +13,111 @@ class Divider(bitWidth: Int) extends Module {
         val remainder = Output(UInt(bitWidth.W))
     })
 
-    val remainder   =   RegInit(0.U(bitWidth.W))       //current remainder
-    val quotient    =   RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
-    val divisor     =   RegInit(0.U(bitWidth.W))         //divisor
-    val dividend     =   RegInit(0.U(bitWidth.W))         //divisor
+    val myRemainder   =   RegInit(0.U(bitWidth.W))       //current remainder
+    val myQuotient    =   RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
+    val myDivisor     =   RegInit(0.U(bitWidth.W))         //divisor
+    val myDividend     =   RegInit(0.U(bitWidth.W))         //dividend
+    val myDone     =   RegInit(false.B)         //dividend
 
     val stepI       =   RegInit ((bitWidth-1).U(log2Ceil(bitWidth).W)); // N cycles count
-    stepI       :=   (bitWidth - 1).U
     // Default Outputs
-    io.remainder    :=  0.U;
-    io.quotient     :=  0.asUInt;
+    io.remainder    :=  io.dividend;
+    io.quotient     :=  0.U;
     io.done         :=  false.B;
 
     
 
-    val start :: calculate :: done :: Nil = Enum(3)
-    val state = RegInit(start);
+    val startState :: calculateState :: doneState :: Nil = Enum (3)
+    val state = RegInit(startState);
 
     switch (state) {
         
-        is (start) {
+        is (startState) {
 
             when (io.start) {
-                stepI       :=   (bitWidth - 1).U
-                io.remainder    :=  0.U;
-                io.quotient     :=  0.asUInt;
-                io.done         :=  false.B;
+                stepI          :=   (bitWidth - 1).U
+                myRemainder    :=  0.U;
 
-                divisor     :=  io.divisor
-                dividend    :=  io.dividend
-                state       :=  calculate
+                for (i <- 0 until bitWidth) {
+                    myQuotient(i) := 0.U
+                }
+
+                myDone        :=  false.B;
+
+                myDivisor     :=  io.divisor
+                myDividend    :=  io.dividend
+            }
+            .otherwise {
+                state       :=  calculateState
             }
         }
 
-        is (calculate) {
+        is (calculateState) {
 
-            when (stepI >= 0.U) {
+            when (!myDone) {
 
-                when (divisor === 0.U) {
-                    remainder := dividend
-                    state := done
+                when (myDivisor === 0.U) {
+                    for (i <- 0 until bitWidth) {
+                    myQuotient(i) := (1.U << bitWidth) - 1.U
+                }
+                    myRemainder := myDividend
+                    state := doneState
                 }
 
-                val intermediateRem =  (remainder << 1.U ) + dividend(stepI)  // R' = 2*R + A[i];
+                .otherwise{
 
-                when (intermediateRem < divisor) {
-                    quotient(stepI) := 0.U
-                    remainder       := intermediateRem
-                }
-                .otherwise {
-                    quotient(stepI) := 1.U
-                    remainder       := intermediateRem - divisor
-                }
+                    val intermediateRem =  (myRemainder << 1.U ) + myDividend(stepI)  // R' = 2*R + A[i];
 
-                when (stepI === 0.U) {
-                    state := done
-                }
-                .otherwise {
-                    stepI := stepI - 1.U
-                }
+                    when (intermediateRem < myDivisor) {
+                        myQuotient(stepI) := 0.U
+                        myRemainder       := intermediateRem
+                    }
+                    .otherwise {
+                        myQuotient(stepI) := 1.U
+                        myRemainder       := intermediateRem - myDivisor
+                    }
 
+                    //printf(p"stepI: $stepI, myDividend: $myDividend, intermediateRem: $intermediateRem, myQuotient: ${myQuotient.asUInt}, remainder: $myRemainder\n")
+                    
+                    when (stepI === 0.U) {
+                        myDone  := true.B
+                    }
+                    .otherwise {
+                        stepI := stepI - 1.U
+                    }
+                }
             }
             .otherwise {
-                state   :=  done
+
+                myDone := true.B
+                state   :=  doneState
             }  
 
         }
 
-        is (done) {
+        is (doneState) {
 
-                io.remainder    :=  remainder;
-                io.quotient     :=  quotient.asUInt;
+            when (!io.start) {
+                io.remainder    :=  myRemainder;
+                io.quotient     :=  myQuotient.asUInt;
                 io.done         :=  true.B;
-                state := start
+            }
+            .otherwise {
+                
+                stepI          :=   (bitWidth - 1).U
+                myRemainder    :=  0.U;
+
+                for (i <- 0 until bitWidth) {
+                    myQuotient(i) := 0.U
+                }
+
+                myDone        :=  false.B;
+
+                myDivisor     :=  io.divisor
+                myDividend    :=  io.dividend
+
+                state := startState
+            }
                 
         }
 
