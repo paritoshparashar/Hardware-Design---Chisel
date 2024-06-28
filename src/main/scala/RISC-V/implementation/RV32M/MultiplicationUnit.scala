9d10b2c4f25367487109536d39cce681ba772a82
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-06-28 15:28:40 +0200

    Multiplication uni - decoder

--- src/main/scala/RISC-V/implementation/RV32M/MultiplicationUnit.scala
+++ src/main/scala/RISC-V/implementation/RV32M/MultiplicationUnit.scala
@@ -5,23 +5,13 @@ import chisel3.util._
 
 import RISCV.interfaces.generic.AbstractExecutionUnit
 import RISCV.model._
-import cats.CoflatMap.ops
 
 
 class MultiplicationUnit extends AbstractExecutionUnit {
 
     io.misa := "b01__0000__0_00000_00000_00100_00000_00000".U
-    
-    val opcode = RISCV_OP(io.instr(6, 0))
-    val funct7 = RISCV_FUNCT7(io.instr(31, 25))
 
-    when (opcode === RISCV_OP.OP) {
-        when (funct7 === RISCV_FUNCT7.MULDIV) {
-            io.valid := true.B
-        }
-    }.otherwise(
-        io.valid := false.B)
-
+    io.valid := false.B    
     io.stall := STALL_REASON.NO_STALL
 
     io_data <> DontCare
@@ -31,5 +21,20 @@ class MultiplicationUnit extends AbstractExecutionUnit {
     
     //TODO: Your solution to Problem 2.4 should go here
 
+    val opcode = RISCV_OP(io.instr(6, 0))
+    val funct7 = RISCV_FUNCT7(io.instr(31, 25))
+    val funct3 = RISCV_FUNCT3(io.instr(14, 12))
+    val noMulButDiv= io.instr(14)
 
+    val RS1 = 0.U
+    val RS2 = 0.U
+    val RD = 0.U
+    
+    when (opcode === RISCV_OP.OP) {
+        
+        when (!noMulButDiv && funct7 === RISCV_FUNCT7.MULDIV) {
+            io.valid := true.B
+
+        }
+    }
 }
